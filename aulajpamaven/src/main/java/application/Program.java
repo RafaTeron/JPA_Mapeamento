a48bf269ea040c47b2ca05989c1503b2eb86b903
Rafael Abreu <rafael_sabreu@hotmail.com>
2023-03-01 21:00:14 -0300

    Find

--- aulajpamaven/src/main/java/application/Program.java
+++ aulajpamaven/src/main/java/application/Program.java
@@ -9,21 +9,26 @@ import dominio.Pessoa;
 public class Program {
 
 	public static void main(String[] args) {
-		
-		Pessoa p1 = new Pessoa(null, "Carlos da Silva", "carlos@gmail.com");
-		Pessoa p2 = new Pessoa(null, "Joaquim Torres", "joaquim@gmail.com");
-		Pessoa p3 = new Pessoa(null, "Ana Maria", "ana@gmail.com");
+		//INSERIR
+		//Pessoa p1 = new Pessoa(null, "Carlos da Silva", "carlos@gmail.com");
+		//Pessoa p2 = new Pessoa(null, "Joaquim Torres", "joaquim@gmail.com");
+		//Pessoa p3 = new Pessoa(null, "Ana Maria", "ana@gmail.com");
 		
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
         EntityManager em = emf.createEntityManager();
         em.getTransaction().begin();;
 
-		em.persist(p1);
-		em.persist(p2);
-		em.persist(p3);
+		//em.persist(p1);
+		//em.persist(p2);
+		//em.persist(p3);
 		
+        Pessoa p = em.find(Pessoa.class, 2);
+        
 		em.getTransaction().commit();
         System.out.println("Pronto!");
+        System.out.println(p);
+        em.close();
+        emf.close();
 	}
 
 }
